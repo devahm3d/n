20986d8dc342d31459db2b2ec3880bdc2d199cf8
devahm3d <162648583+devahm3d@users.noreply.github.com>
2024-03-16 16:12:58 +0200

    Add files via upload

---
+++ mody.py
@@ -0,0 +1,19 @@
+import os
+
+
+class Mody(object):
+    ELHYBA = os.environ.get("A7_M3", "")
+    
+    OWNER = os.environ.get("OWNER", "5449190469")
+    
+    ID_BOT = int(os.environ.get("ID_BOT", ""))
+    
+    USER_BOT = os.environ.get("USER_BOT", "")
+    
+    MAX_ACCOUNTS = int(os.environ.get("MAX_ACCOUNTS", ""))
+    
+    API_ID = os.environ.get("API_ID", "12822843")
+    
+    API_HASH = os.environ.get("API_HASH", "689f3bfcefe86a6a3dffe6830baf1010")
+    
+
